707d2a9e97baab8c73c956296a41abfb7d2d387e
Sushmanth Reddy <sushmanthreddy@Sushmanths-MacBook-Air.local>
2026-03-12 18:33:55 +0530

    feat: directory setup

--- src/server.ts
+++ src/server.ts
@@ -12,22 +12,30 @@ dotenv.config();
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:4200', 'https://smart-expense-frontend-one.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use('/api/auth', authRoutes);
-app.use('/api/expenses', expenseRoutes);
+
 
 // A simple test route to prove the server is awake
 app.get('/api/health', (req, res) => {
   res.json({ message: 'TypeScript Backend is ALIVE!' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
+
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/expense_tracker'; // Fallback just in case .env is missing
 
 // Connect to Database and start server
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('✅ Connected to MongoDB');
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
   })
   .catch((error) => {
     console.error('❌ MongoDB connection error:', error);
